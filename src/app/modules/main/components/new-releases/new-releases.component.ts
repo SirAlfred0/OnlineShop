3e3a641270644f36613d8f8b368f2e50778d0fcf
Mohammad Asadi <rasoul1551@gmail.com>
2023-06-10 11:44:00 +0330

    Feat NewReleases

--- src/app/modules/main/components/new-releases/new-releases.component.ts
+++ src/app/modules/main/components/new-releases/new-releases.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Category } from 'src/app/models/gift-suggestion.model';
+import { NewProduct, NewProducts } from 'src/app/models/new-product.model';
+import { CommonHttpService } from 'src/app/services/business/common-http.service';
 
 @Component({
   selector: 'app-new-releases',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-releases.component.css']
 })
 export class NewReleasesComponent {
+  
+  minPrice: number = 0;
+  maxPrice: number = 10000;
+  categories: Category[] = [];
+  products: NewProduct[] = [];
 
+  filterForm: FormGroup = new FormGroup({
+    MinPrice: new FormControl(this.minPrice),
+    MaxPrice: new FormControl(this.maxPrice),
+    CategoryId: new FormControl([]),
+  });
+
+
+  constructor(
+    private commonHttpService: CommonHttpService
+  ){}
+
+
+  ngOnInit(): void {
+    this.getNewProducts();
+  }
+
+  getNewProducts(): void
+  {
+    this.commonHttpService.getNewProducts().subscribe({
+      next: (res: NewProducts) => {
+        this.maxPrice = res.maxPrice;
+        this.minPrice = res.minPrice;
+        this.categories = res.categories;
+        this.products = res.products;
+      }
+    });
+  }
+
+  displayPriceWith(value: number): string
+  {
+    return value + '$';
+  }
 }
